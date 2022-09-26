49bddaa21d31df84b9de57a11b530698026869b9
Bharath-SR <bharath.s0941@gmail.com>
2022-09-26 07:55:39 +0530

    UC1:Adding Elments

--- BinarySearchTree/src/com/bridgeLabz/BST.java
+++ BinarySearchTree/src/com/bridgeLabz/BST.java
@@ -12,9 +12,46 @@ public class BST {
 	/**
 	 * Started BST
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	static class Node {    
+	    int value; //data stored as type int
+	        Node left, right; //reference to left and right pointer
+			public String key;
+	          
+	        Node(int value){ //constructor
+	            this.value = value; 
+	            // store null value for left and right child
+	            left = null; 
+	            right = null; 
+	        }
 	}
+	// class to create nodes
+	class Node1 {
+	  int key;
+	  Node left, right;
+
+	  public void Node(int item) {
+	  key = item;
+	  left = right = null;
+	  }
+	}
+
+	//class for binary tree
+	class BinaryTree {
+	  Node root;
+
+	  // Traverse tree
+	  public void traverseTree(Node node) {
+	    if (node != null) {
+	      traverseTree(node.left);
+	      System.out.print(" " + node.key);
+	      traverseTree(node.right);
+	    }
+	  }
+
+	
+	}
+	
+
 
 }
