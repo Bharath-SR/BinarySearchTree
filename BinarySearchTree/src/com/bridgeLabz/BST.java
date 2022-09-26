587e2e6f11927002d34789bde26dc715b6f3a87a
Bharath-SR <bharath.s0941@gmail.com>
2022-09-26 07:46:00 +0530

    Staretd: Binary Search tree

---
+++ BinarySearchTree/src/com/bridgeLabz/BST.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.bridgeLabz;
+
+/**
+ * @author Admin
+ *
+ */
+public class BST {
+
+	/**
+	 * Started BST
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
